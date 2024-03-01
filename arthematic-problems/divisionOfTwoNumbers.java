e4fa7904abe7139d177f355e23f3c41102fa2079
faisal <faisalfnadaf@gmail.com>
2024-03-01 20:47:06 +0530

    arti division

---
+++ arthematic-problems/divisionOfTwoNumbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class divisionOfTwoNumbers  {
+
+    public static void main(String args[]) {
+            Scanner scan = new Scanner(System.in);   //scan is variable
+            System.out.println("Enter Two Numbers");
+            int a =scan.nextInt();
+            int b =scan.nextInt();
+            int sum=a/b;
+          System.out.println(sum);
+        
+     
+}
+}
