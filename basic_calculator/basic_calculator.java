eeb20dfef73d4811ac0c90b9c179fa12f8b83e22
faisal <faisalfnadaf@gmail.com>
2024-03-02 21:08:52 +0530

    coded basic calculator

---
+++ basic_calculator/basic_calculator.java
@@ -0,0 +1,47 @@
+package basic_calculator;
+
+import java.util.*;
+
+public class basic_calculator {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in); // scan is variable
+        System.out.print("Enter a = ");
+        int a = scan.nextInt();
+        System.out.print("Enter b = ");5
+
+        int b = scan.nextInt();
+        System.out.print("Enter operator = ");
+        char operator = scan.next().charAt(0);
+
+        switch (operator) {
+
+            case '+':
+                System.out.println(a + b);
+
+                break;
+            case '-':
+                System.out.println(a - b);
+
+                break;
+            case '/':
+                System.out.println(a / b);
+
+                break;
+            case '*':
+                System.out.println(a * b);
+
+                break;
+            case '%':
+                System.out.println(a % b);
+
+                break;
+
+            default:
+                System.out.println("invalid operator");
+
+                break;
+        }
+
+    }
+
+}
